6d5d7aaa23dc34e5c34952b0c6c0b7c8e2049268
Indrajit Pal <61076234+indropal@users.noreply.github.com>
2023-01-05 11:57:43 +0530

    Recent Practice Update as of 5th Jan 2023

--- Daily Challenges/453_Minimum_Number_of_Arrows_to_Burst_Balloons.py
+++ Daily Challenges/453_Minimum_Number_of_Arrows_to_Burst_Balloons.py
@@ -1,5 +1,8 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        """ 
+        # INITIAL SOLUTION ~ Time Complexity - O(N^2) -> was ACCEPTED
+
         # The problem involves balloons represented as a 2D array of integers
         # which represent the X-axis range each ballon takes up
         # An arrow can pop multiple balloons in its path.
@@ -34,7 +37,7 @@ class Solution:
                      ((points[i][0] <= points[j][0]) and (points[j][-1] <= points[i][-1]))
                     ):
                     # merge ranges at i & j:
-                    # CONSIDER ALL MERGE CONDITIONS
+                    # CONSIDER ALL MERGE CONDITIONS ~ you had trouble thinking of all the conditions for MERGE
                     # update the limits of the merged ranges to consider the intersection between them
                     # such that an arrow shot at this intersected range will pop balloon
                     points[i][-1] = min(points[i][-1], points[j][-1])
@@ -51,3 +54,27 @@ class Solution:
         
         # print(points)
         return len(points)
+        """
+
+        # There is a more optimal solution. The previously submitted solution was geared more towards
+        # simulating merging of intervals. However, we can do a better job without simulation
+        # Time Complexity: O(N)
+
+        # Sort the ranges on the basis of the ending index
+        # Iterate through the array by comparing the maximum end-index of ranges traversed so far
+        # with the consecutive start-indices
+        # If the max. end-index so far is less than the next range's start-index then we'd need a new arrow
+        # or else a single arrow can take the balloon down
+
+        points = sorted(points, key= lambda p: p[-1]) # the strt-index is NOT guaranteed to be sorted
+
+        ans = 1 # initialize with 1 arrow as we would need atmost number of balloons equivalent arrows to pop them
+        reach, N = points[0][-1], len(points) # store the maximum end-index that can be reached for the first index
+
+        for i in range(1, N):
+            if reach < points[i][0]:
+                # we need a new arrow as the balloon starts from a disjoint range
+                ans += 1
+                reach = points[i][-1] # update the reachas the max. the new arrow can reach is the end index of the i Range
+
+        return ans
